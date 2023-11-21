8328175c85e9698b737a7e48dff41a67303fa66d
Saurabh Talele <saurabhatalele@gmail.com>
2023-11-22 00:00:18 +0530

    changed homepage design and added features

--- Components/Home/HomePage.jsx
+++ Components/Home/HomePage.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image"
 import styles from "./HomePage.module.css"
 import { motion } from "framer-motion"
+import Link from "next/link"
+
 
 const HomePage = () => {
   return (
-
+<>
+      <div className={styles.bg}></div>
     <div className={styles.home__container}>
       <div className={styles.intro}>
 
@@ -71,14 +74,49 @@ The smartest <span className={styles.unique__text}>AI resume builder</span></h1>
         <Image src={'/SampleResume.jpg'} width={1000} height={1000} className={styles.sample__resume} alt="resume sample"/>
       </div>
 
+      <section className={styles.features__section}>
+        <h2 className={styles.section__heading}>Features</h2>
+        <div className={styles.features__container}>
+          <div className={styles.feature__tile}>
+            <Image src={'/icons/suggestion.png'} width={64} height={64} alt="AI" className={styles.feature__image}/>
+            <div className={styles.feature__details}>
+            <h3 className={styles.feature__heading}>AI Content</h3>
+            <p className={styles.feature__desc}>AI Resume Builder uses AI to generate resume content based on your profile.</p>
+            </div>
+          </div>
+          <div className={styles.feature__tile}>
+            <Image src={'/icons/template.png'} width={64} height={64} alt="Templates" className={styles.feature__image}/>
+            <div className={styles.feature__details}>
+            <h3 className={styles.feature__heading}>10+ templates</h3>
+            <p className={styles.feature__desc}>Choose from 10+ templates to create your resume.</p>
+            </div>
+            </div>
+          <div className={styles.feature__tile}>
+            <Image src={'/icons/easy.png'} width={64} height={64} alt="Easy" className={styles.feature__image} />
+            <div className={styles.feature__details}>
+            <h3 className={styles.feature__heading}>Easy interface</h3>
+            <p className={styles.feature__desc}>A user-friendly interface that guides users through the resume-building process.</p>
+            </div>
+            </div>
+
+          <div className={styles.feature__tile}>
+            <Image src={'/icons/preview.png'} width={64} height={64} alt="Free" className={styles.feature__image}/>
+            <div className={styles.feature__details}>
+            <h3 className={styles.feature__heading}>Dynamic Editing and Preview</h3>
+            <p className={styles.feature__desc}>Real-time preview of the resume as users make edits. </p>
+            </div>
+            </div>
+        </div>
+      </section>
+
       <footer className={styles.footer}>
-        <Image src={'/HomeImage.png'} width={200} height={150}></Image>
+        <Image src={'/images/sapiens.svg'} width={300} height={200}></Image>
         <div>
           <ul className={styles.links}>
             <li>Home</li>
             <li>Pricing</li>
             <li>About</li>
-            <li>Login</li>
+            <Link href={"/login"}>Login</Link>
           </ul>
         </div>
 
@@ -96,6 +134,8 @@ The smartest <span className={styles.unique__text}>AI resume builder</span></h1>
 
       
     </div>
+</>
+
   )
 }
 
